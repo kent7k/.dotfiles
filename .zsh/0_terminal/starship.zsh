c405f68af2abfb0f0b2a68d7079edebbe9f19ba5
kent7k <kent.ki720@gmail>
2023-01-08 19:49:00 +0900

    add zsh about terminal

---
+++ .zsh/0_terminal/starship.zsh
@@ -0,0 +1,4 @@
+# config about spaceship-prompt
+eval "$(starship init zsh)"
+
+export STARSHIP_CONFIG="${DOT_FILES}/3_packages/starship/starship.toml"
---
+++ .zsh/0_terminal/tmux-fzf.zsh
@@ -0,0 +1,2 @@
+export FZF_TMUX_OPTS="-p 80%"
+export FZF_TMUX=1
