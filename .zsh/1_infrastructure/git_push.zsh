deae326127be82032e3b23e2d0f3be9c530b6998
kent7k <kent.ki720@gmail>
2023-02-12 21:13:04 +0900

    add git_push

--- .zsh/1_infrastructure/git_push.zsh
+++ .zsh/1_infrastructure/git_push.zsh
@@ -3,11 +3,13 @@ alias gip='git pull'
 
 alias gia='git_add'
 function git_add() {
+  echo
   added_files=$(git diff --name-only | fzf --multi)
   if [ -n "$added_files" ]; then
+    echo -e "$(c_light_gray "Added file:")"
     while read -r file; do
       git add "$file"
-      echo -e "$(c_light_gray "Added file:") $(c_cyan "$added_files")"
+    echo -e "$(c_cyan "    $added_files")"
     done <<< "$added_files"
     use_commit
   else
@@ -16,7 +18,8 @@ function git_add() {
 }
 
 function use_commit() {
-        echo "Do you want to commit now (Y/n)?"
+  echo
+  echo "$(c_green "Do you want to commit files above?") (Y/n)"
         read -r CONFIRM_CONFIRM;
   if [ "$CONFIRM_CONFIRM" = "y" ] || [ -z "$CONFIRM_CONFIRM" ]; then
       git_commit
@@ -27,10 +30,10 @@ function use_commit() {
 
 alias gic='git_commit'
 function git_commit() {
-       # gic = git commit.
-       echo "What's the commit name?"
+       echo
+       echo "$(c_green "What's the commit name?") (Y/n)"
        read -r COMMIT_NAME;
-       echo "Do you want to use --no-verify option? (Y/n)"
+       echo "$(c_green "Do you want to use --no-verify option?") (Y/n)"
        read -r VERIFY
        if [ "$VERIFY" = "y" ] || [ -z "$VERIFY" ]; then
          git commit -m "$COMMIT_NAME" --no-verify
