1b972dc3436c69a61f54346a0ff7836c61e529fe
kent7k <kent.ki720@gmail.com>
2023-02-14 22:07:06 +0900

    Delete git.zsh

--- .zsh/1_infrastructure/git/git_completion.zsh
+++
@@ -1,7 +0,0 @@
-# Set GitHub CLI setting for completion
-eval "$(gh completion -s zsh)"
-
-# Makes git auto completion faster favouring for local completions
-__git_files() {
-  _wanted files expl 'local files' _files
-}
--- .zsh/1_infrastructure/git/git_current_branch.zsh
+++
@@ -1,4 +0,0 @@
-function call_current_branch() {
-  CURRENT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
-  echo "$CURRENT_BRANCH"
-}
