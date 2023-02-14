4b5a9adf01c288b65323fe8ebd2a690b002c40e7
kent7k <kent.ki720@gmail.com>
2023-02-14 22:04:26 +0900

    Modify Zsh Dir

--- .zsh/0_terminal/advanced-cd.zsh
+++ .zsh/0_terminal/advanced-cd.zsh
@@ -20,14 +20,15 @@ alias cddd='fzf-cdr'
 function fzf-cdr() {
   target_dir=$(cdr -l | sed 's/^[^ ][^ ]*  *//' | fzf)
   if [ -n "$target_dir" ]; then
-    cd "${target_dir/#\~/$HOME}"
+    cd "${target_dir/#\~/$HOME}" || exit
   fi
 }
 
 # cdr - cd to selected directory of repository
 alias cdr='fzf-ghq'
 function fzf-ghq() {
-  local src=$(ghq list | fzf --preview "ls -laTp $(ghq root)/{} | tail -n+4 | awk '{print \$9\"/\"\$6\"/\"\$7 \" \" \$10}'")
+  local src
+  src=$(ghq list | fzf --preview "ls -laTp $(ghq root)/{} | tail -n+4 | awk '{print \$9\"/\"\$6\"/\"\$7 \" \" \$10}'")
   if [ -n "$src" ]; then
     BUFFER="cd $(ghq root)/$src"
     zle accept-line
@@ -40,7 +41,7 @@ bindkey '^]' ghq-fzf
 # fd - cd to selected directory
 # Note: find command makes a list from current to the lower layer,
 #       then pass stdout to fzf, and finally pass the result to cd command.
-fd() {
+function fd() {
   local dir
   dir=$(find ${1:-.} -path '*/\.*' -prune \
     -o -type d -print 2>/dev/null | fzf +m) &&
--- .zsh/base.zsh
+++ .zsh/base.zsh
@@ -1,2 +1,3 @@
 export BREW_PATH="${DOT_FILES}/3_packages/.brewfile"
+
 export TMUX_CONF_PATH="${DOT_FILES}/0_terminal/tmux/conf"
