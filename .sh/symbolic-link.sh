4f8454d509cd1e83b7a42c813bfcda515cae71b6
Kento <46371150+kent7k@users.noreply.github.com>
2023-03-04 12:17:17 +0900

    Refactor symbolic link (#25)

--- .sh/symbolic-link.sh
+++ .sh/symbolic-link.sh
@@ -4,22 +4,12 @@
 declare -a LINKS=(
   ".tmux/.tmux.conf" "$HOME/.tmux.conf"
   ".zsh/.zshrc" "$HOME/.zshrc"
-<<<<<<< HEAD
-  "0_terminal/.alacritty.yml" "$HOME/.config/.alacritty.yml"
-  "0_terminal/tmux/.tmux.conf" "$HOME/.tmux.conf"
-  "0_terminal/nvim/init.vim" "$HOME/.config/nvim/init.vim"
-  "0_terminal/nvim/coc-settings.json" "$HOME/.config/nvim/coc-settings.json"
-  "1_infrastructure/.gitconfig" "$HOME/.gitconfig"
-  "2_languages/.tool-versions" "$HOME/.tool-versions"
-  "3_packages/.crates.toml" "$HOME/.cargo/.crates.toml"
-=======
   ".zsh/0_terminal/.alacritty.yml" "$HOME/.config/.alacritty.yml"
   ".zsh/0_terminal/.nvim/init.vim" "$HOME/.config/nvim/init.vim"
   ".zsh/0_terminal/.nvim/coc-settings.json" "$HOME/.config/nvim/coc-settings.json"
   ".zsh/1_infrastructure/git/.gitconfig" "$HOME/.gitconfig"
   ".zsh/2_languages/asdf/.tool-versions" "$HOME/.tool-versions"
   ".zsh/3_packages/.crates.toml" "$HOME/.cargo/.crates.toml"
->>>>>>> 1a0fb2a (Arrange Directories 230304)
 
   #  FIXME
   #  "3_packages/tealdeer/.tealdeer.toml" "$HOME/Library/Application Support/tealdeer"
--- .zsh/3_packages/asdf.zsh
+++ .zsh/3_packages/asdf.zsh
@@ -1,10 +1,6 @@
 # Set asdf's config.
 . "$HOME"/.asdf/asdf.sh
-<<<<<<< HEAD
 export ASDF_CONFIG_FILE="$ASDF_CONFIG_PATH"
-=======
-export ASDF_CONFIG_FILE="${DOT_FILES}/.zsh/2_languages/.asdfrc"
->>>>>>> 1a0fb2a (Arrange Directories 230304)
 
 # append completions to fpath
 fpath=(${ASDF_DIR}/completions $fpath)
