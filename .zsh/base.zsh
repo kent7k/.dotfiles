3c2c3e5ecc07d224943015f03da0d5db944317f2
kent7k <kent.ki720@gmail.com>
2023-03-05 09:52:00 +0900

    Modify Config of navi

--- .zsh/base.zsh
+++ .zsh/base.zsh
@@ -1,3 +1,8 @@
+# Config about Base
 export BREW_PATH="${DOT_FILES}/.zsh/3_packages/.brewfile"
 
+# Config about Language
 export ASDF_CONFIG_PATH="${DOT_FILES}/.zsh/2_languages/.asdfrc"
+
+# Config about Package
+export NAVI_CONFIG="${DOT_FILES}/.zsh/0_terminal/.navi.yaml"
