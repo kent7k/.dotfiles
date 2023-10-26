c451f3848467005efa15bea58d8881eb66ea13c8
kent7k <kent.ki720@gmail.com>
2023-10-26 10:53:29 +0900

    Modify git_commit

---
+++ .zsh/0_terminal/base.zsh
@@ -0,0 +1,9 @@
+BOLD=$(tput bold)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+NORMAL=$(tput sgr0)
+RED=$(tput setaf 1)
+
+echo_section() {
+    printf "\n%s%s► %s%s\n%s-----------------------------------------------%s\n" "${BOLD}" "${GREEN}" "$1" "${NORMAL}" "${YELLOW}" "${NORMAL}"
+}
--- .zsh/1_infrastructure/git/git_commit.zsh
+++ .zsh/1_infrastructure/git/git_commit.zsh
@@ -1,12 +1,3 @@
-BOLD=$(tput bold)
-GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
-NORMAL=$(tput sgr0)
-RED=$(tput setaf 1)
-
-echo_section() {
-    printf "\n%s%s► %s%s\n%s-----------------------------------------------%s\n" "${BOLD}" "${GREEN}" "$1" "${NORMAL}" "${YELLOW}" "${NORMAL}"
-}
 
 alias gha='git_add'
 function git_add() {
