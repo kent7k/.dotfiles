11434ae95bb4c9a8ff4dec1d232c4242e4e06bbe
kent7k <${GIT_AUTHOR_PRIVATE_EMAIL}>
2023-10-29 17:48:49 +0900

    Revert "A"
    This reverts commit 07774209f76c9322237bd1fde87f96308e4fbd42.

--- .zsh/0_terminal/swift.zsh
+++
@@ -1,7 +0,0 @@
-function swift_format() {
-	git diff --name-only origin/develop |
-		grep swift |
-		while read l; do
-			/opt/homebrew/bin/swiftformat --config "$HOME/.dotfiles/.lint/.swiftformat" "$l"
-		done
-}
